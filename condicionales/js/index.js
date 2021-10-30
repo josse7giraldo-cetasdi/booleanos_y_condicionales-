4d0807466570dab280150b4bfc2404bba1c18566
josse7giraldo <78995215+josse7giraldo@users.noreply.github.com>
2021-10-30 15:09:15 -0500

    Add files via upload

---
+++ condicionales/js/index.js
@@ -0,0 +1,9 @@
+
+let numero_comparar = 100
+
+if (numero_comparar > 100)  {    
+  document.write ("El valor de la variable es mayor que 100");
+} else {
+document.write ("El valor de la variable es menor o igual que 100")
+}
+
